019b56c69db25024f8917903727d50bb8b867b7d
Yuran Germano Mahige <yurangermano06@gmail.com>
2026-06-02 16:13:53 +0200

    add banner

---
+++ Repo-Local/script.js
@@ -0,0 +1,9 @@
+let banners =
+["De Mocambique para o mundo", "Ola, Mundo!"];
+let bannerActual = 0;
+function trocaBanner() {
+bannerActual = (bannerActual + 1) % 2;
+document.querySelector('#mensagem').textContent =
+banners[bannerActual];
+}
+setInterval(trocaBanner, 2000);
